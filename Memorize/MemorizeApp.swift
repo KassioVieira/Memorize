5d9c3de230828b9d64b4acfcd11b2b7e67bd305c
KassioVieira <kassio.vieira7@gmail.com>
2020-12-08 18:32:51 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kássio Luz on 08/12/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
